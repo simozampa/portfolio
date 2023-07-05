7a4296a18d2f372fd2a3a838e32a5629a2d52e6c
Simone Zamparini <simozampa@ucla.edu>
2023-07-04 18:14:49 -0700

    Basic page and header

---
+++ portfolio/app/(components)/header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <div className="fixed w-full top-0 shadow-xl">
+            <div className="flex justify-between items-center w-full h-full">
+                <span>Simone Zamparini</span>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
