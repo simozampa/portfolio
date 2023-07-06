054d2829fa2b90807f532b898fd88960a509aedd
Simone Zamparini <simozampa@ucla.edu>
2023-07-05 23:49:29 -0700

    Dynamic navbar and pages

--- portfolio/app/(components)/header.tsx
+++ portfolio/app/(components)/header.tsx
@@ -1,12 +1,42 @@
+import Link from "next/link"
+import { headers } from "next/headers";
+const navigation = [
+    { name: "HOME", href: "/" },
+    { name: "ABOUT", href: "/about" },
+    { name: "SKILLS", href: "/skills" },
+    { name: "PROJECTS", href: "/projects" },
+    { name: "CONTACT", href: "/contact" }
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
 export default function Header() {
+
+    const headersList = headers();
+    const activePath = headersList.get("x-invoke-path");
+
     return (
-        <div className="fixed w-full top-0 shadow-xl">
-            <div className="flex justify-between items-center mx-10 mt-4">
-                <span>Simone Zamparini</span>
+        <div className="fixed w-full top-0">
+            <div className="flex justify-between items-center mx-10 pt-4">
+                <span className="text">Simone Zamparini</span>
                 <ul>
-                    <li>
-                        Home
-                    </li>
+                    <div className="flex items-center space-x-10">
+                        {navigation.map(item => (
+                            <li key={item.name}
+                                className={
+                                    classNames(
+                                        activePath == item.href ? 'border-b-2 border-gray-900 text-gray-900 font-semibold'
+                                            : 'hover:font-medium',
+                                        'pb-2 text-sm')}
+                            >
+                                <Link href={item.href}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </div>
                 </ul>
             </div>
         </div>
